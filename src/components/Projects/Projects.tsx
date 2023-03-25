80c47927a2fa9df6774b47b25f5e2d5d0fa78a63
Bartosz <bartoszmrosek@op.pl>
2023-03-25 23:38:06 +0100

    Skills section content finished

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -105,7 +105,7 @@ const Projects: React.FC = () => {
             }
           });
         },
-        { rootMargin: "0px", threshold: 0.7 }
+        { rootMargin: "0px", threshold: [0.25, 0.75] }
       );
       if (
         imageRefs.every((imageRef) => imageRef.current !== null) &&
